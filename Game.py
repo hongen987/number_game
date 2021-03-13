1f946c806c11544dc0022fb751a75ccbc039eef1
Allen <0912361632allen@gmail.com>
2021-03-13 16:24:39 +0800

    final_commit

--- Game.py
+++ Game.py
@@ -1,9 +1,12 @@
 import random
-x = random.randint(1,100)
+a = input('輸入開始值:')
+b = input('輸入結束值:')
+a = int(a)
+b = int(b)
+x = random.randint(a, b)
 z = 0
-print(x)
 while True:
-    y = input('請輸入從1~100的其中一個數字')
+    y = input('請猜1個數字')
     y = int(y)
     z = z + 1
     if y == x:
@@ -11,9 +14,9 @@ while True:
         print('你猜了', z, '次')
         break
     else:
-        if x > y and y >= 1: 
+        if x > y and y >= a: 
             print('比這個數字大')
-        elif x < y and y <= 100:
+        elif x < y and y <= b:
             print('比這個數字小')
         else:
-            print('只能輸入1~100的數')
+            print('只能輸入從', a,'到', b, '的其中一個數字')
